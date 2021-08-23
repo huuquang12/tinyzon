7ec4fb29e973fe35a730f46ac44e1d3942bb34ee
Huu Quang <h.quang1200@gmail.com>
2021-08-24 01:15:12 +0700

    Build function addToCart and display CartPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage'
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
 <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/"> 
               amazona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+          <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
           </header>
         <main>
+          <Route path="/cart/:id?" component={CartPage}></Route>
           <Route path="/product/:id" component={ProductPage}></Route>
           <Route path="/" component={HomePage} exact></Route>
         </main>
