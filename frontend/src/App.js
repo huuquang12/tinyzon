aaa48be0027c1b62cbe3232e1d3c585d1bb1a0da
Huu Quang <h.quang1200@gmail.com>
2021-08-24 16:58:17 +0700

    Create SigninPage and function signin and signout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage'
+import SigninPage from './pages/SigninPage';
+import { signout } from './actions/userActions';
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
 
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  }
+
   return (
-<BrowserRouter>
+    <BrowserRouter BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
@@ -25,13 +36,30 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">
+                      {userInfo.name} 
+                        <i className="fa fa-caret-down">
+                        </i> 
+                  </Link>
+                  <ul className="dropdown-content">
+                    <Link to="#signout" onClick={signoutHandler}>Sign out</Link>
+                  </ul>
+                </div>
+              ) : 
+              (
+                <Link to="/signin">Sign In</Link>
+              )
+            }
           </div>
           </header>
         <main>
           <Route path="/cart/:id?" component={CartPage}></Route>
           <Route path="/product/:id" component={ProductPage}></Route>
           <Route path="/" component={HomePage} exact></Route>
+          <Route path="/signin" component={SigninPage}></Route>
         </main>
         <footer className="row center">All right reserved</footer>
       </div>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,18 +5,25 @@ import {
     productListReducer,
   } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userSigninReducer } from './reducers/userReducers';
   
 const initialState = {
-    cart: {
-      cartItems: localStorage.getItem('cartItems')
-        ? JSON.parse(localStorage.getItem('cartItems'))
-        : [],
-    },
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo') ?
+      JSON.parse(localStorage.getItem('userInfo')) :
+      null
+  },
+  cart: {
+    cartItems: localStorage.getItem('cartItems') ?
+      JSON.parse(localStorage.getItem('cartItems')) :
+      [],
+  },
 };
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userSignin: userSigninReducer
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
